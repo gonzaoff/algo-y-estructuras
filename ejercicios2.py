97f9dc6e0887b28924bc02685cadb063fed9f33b
Gonzalo <sibolichgonzalo@gmail.com>
2023-09-10 18:10:54 -0300

    Valor absoluto y Resolvente

--- ejercicios2.py
+++ ejercicios2.py
@@ -7,10 +7,13 @@ system("cls")
 
 
 def Factorial():
+
     #input: Numero desde 100 hasta 1.000.000
     #output: Factorial del numero.
+
     numero=int(input("Ingrese el numero (desde 100 hasta 1.000.000) del que desea saber el factorial: "))
     condicion= numero >= 100 and numero <= 1000000
+
     if condicion:
         factorialPos= numero
         factorialNeg= numero * (-1)
@@ -21,8 +24,14 @@ def Factorial():
 # Factorial()
 
 def Resolvente(A,B,C):
-    #input:valores "a,b,c" que se reemplacen en una resolvente
+    #input:valores "A,B,C" que se reemplacen en una resolvente.
+
+    #A=Variable cuadratica
+    #B=Variable lineal
+    #C=Variable independiente
+
     #output: resultado de la resolvente.
+
     x1=int()
     x2=int()
     d1=B**2-(4*A*C)
@@ -34,24 +43,26 @@ def Resolvente(A,B,C):
         x1 = -B+isqrt(d1)/(2*A)
         x2 = -B-isqrt(d2)/(2*A)
         print(f"el valor de la raiz x1: -1*{x1}\nel valor de la raiz x2: -1*{x2}")
-    
-    if d1<0:
+        print("El valor de las raices pertene a los numeros complejos.")
+
+    elif d1<0:
         d1 = d1*(-1)
         x1 = -B+isqrt(d1)/(2*A)
         x2 = -B-isqrt(d2)/(2*A)
         print(f"el valor de la raiz x1: -1*{x1}\nel valor de la raiz x2: {x2}")
-    
-    if d2<0:
+        print("el valor de la raiz x1 pertenece a los numeros complejos.")
+
+    elif d2<0:
         d2 = d2*(-1)
         x1 = -B+isqrt(d1)/(2*A)
         x2 = -B-isqrt(d2)/(2*A)
         print(f"el valor de la raiz x1: {x1}\nel valor de la raiz x2: -1*{x2}")
+        print("el valor de la raiz x1 pertenece a los numeros complejos.")
 
-    if d1>0 and d2>0:
-        print("¡¡Si ves cuatro (4) valores de raiz puede que el resultado pertenezca los numeros complejos!!")
+    elif d1>0 and d2>0:
         x1 = -B+isqrt(d1)/(2*A)
         x2 = -B-isqrt(d2)/(2*A)
         print(f"el valor de la raiz x1: {x1}\nel valor de la raiz x2: {x2}")
 
-# Resolvente(22,7,3)
+Resolvente(-22,7,-3)
 
